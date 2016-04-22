b152401f41f37d3b26bda4d01c22bf5ce160960d
Hyun Jeong Hong <hisuida@me.com>
2016-04-22 15:14:08 -0400

    FPF-17 Added bootsrap theme

--- FoodieBFCapstone/FoodieBFCapstone/App_Start/BundleConfig.cs
+++ FoodieBFCapstone/FoodieBFCapstone/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace FoodieBFCapstone
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"
+                    ));
         }
     }
 }
