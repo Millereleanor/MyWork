aa44a1bd0ec6baa2d2a5b3a57fd6b0f3a43835f5
James Spencer <jspencer@virtualhold.com>
2016-05-02 10:10:10 -0400

    Minor refactor to load TinyMCE scripts first, and moved custom JS Script for Tag-It into app folder

---
+++ FoodieBFCapstone/FoodieBFCapstone.UI/Scripts/App/tag-input/tag-input-app.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    var sampleTags = ['Figure', 'Out', 'How', 'To', 'Load'];
+    $('#singleFieldTags').tagit({
+        availableTags: sampleTags,
+        singleField: true,
+        allowSpaces: true,
+        singleFieldNode: $('#mySingleField')
+    });
+});
