f71b96ea61e3cbfecf6fe1141acea6c1adda6d7c
James Spencer <jspencer@virtualhold.com>
2016-05-04 19:08:55 -0400

    FBF-66 Refactoring GetBlogByStatus to resolve failed test

--- FoodieBFCapstone/FoodieBFCapstone.Data/BlogPostRepository.cs
+++ FoodieBFCapstone/FoodieBFCapstone.Data/BlogPostRepository.cs
@@ -69,8 +69,11 @@ namespace FoodieBFCapstone.Data
             blog.MainPictureUrl = dr["MainPictureUrl"].ToString();
             blog.Subcategory.SubcategoryName = dr["SubCategory"].ToString();
             blog.CreatedOn = (DateTime)dr["CreatedOn"];
-            blog.ApprovedOn = (DateTime)dr["ApprovedOn"];
-           
+            DateTime date;
+            DateTime.TryParse(dr["ApprovedOn"].ToString(), out date);
+            if (date != DateTime.MinValue)
+                blog.ApprovedOn = date;
+
 
             return blog;
         }
